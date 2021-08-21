48079cc2c54f5904f67fcc5a082d59abc82cfa38
Nicolas FLAN <nflan@e2r1p7.42.fr>
2021-08-21 11:38:26 +0200

    rush02 save

---
+++ ft_rush02.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rush02.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nflan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/21 10:15:08 by nflan             #+#    #+#             */
+/*   Updated: 2021/08/21 11:11:06 by nflan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_RUSH02_H
+# define FT_RUSH02_H
+
+struct s_dict
+{
+	int		nb;
+	char	*value;
+}	t_dict;
+int		ft_atoi(char *str);
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+
+#endif
---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nflan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/21 10:22:03 by nflan             #+#    #+#             */
+/*   Updated: 2021/08/21 11:11:04 by nflan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_rush02.h"
+int		ft_atoi(char *str);
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+
+int	main(int ac, char **av)
+{
+	t_dict *tab;
+
+	if (ac < 2 || ac > 3)
+		return (0);
+	if (ac == 2)
+	{
+		if (ft_atoi(av[1]) < 0)
+			ft_putstr("Error\n");
+		t_dict = 
+	}
+	if (ac == 3)
+	{
+		if (ft_atoi(av[2]) < 0)
+			ft_putstr("Error\n");
+		t_dict =
+	}
+	return (0);
+}
