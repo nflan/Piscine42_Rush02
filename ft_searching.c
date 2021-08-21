bdbfacce3b0d8819400d85c470076274e41b4486
Nicolas FLAN <nflan@e3r7p9.42.fr>
2021-08-21 20:12:30 +0200

    searching

--- ft_searching.c
+++ ft_searching.c
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rush02.h                                        :+:      :+:    :+:   */
+/*   ft_searching.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nflan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/21 10:15:08 by nflan             #+#    #+#             */
-/*   Updated: 2021/08/21 12:10:51 by nflan            ###   ########.fr       */
+/*   Created: 2021/08/21 18:59:00 by nflan             #+#    #+#             */
+/*   Updated: 2021/08/21 19:17:00 by nflan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_RUSH02_H
-# define FT_RUSH02_H
+#include "ft_rush02.h"
 
-struct s_dict
+int	ft_searching(t_dict *tab, char *str)
 {
-	char	*nb;
-	char	*value;
-}	t_dict;
-int		ft_atoi(char *str);
-void	ft_putstr(char *str);
-void	ft_putchar(char c);
-int		ft_strlen(char *str);
+	unsigned int	i;
 
-#endif
+	i = 0;
+	while (tab[i].nbr)
+	{
+		if (tab[i].nbr == str)
+		{
+			ft_putstr(tab[i].value);
+			return (1);
+		}
+		i++;
+	}
+	ft_putstr("Dict Error\n");
+	return (0);
+}
